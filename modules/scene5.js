7288700591f2a744cc983948dd4fc60939020182
Chris Brenton <chrisbrenton@gmail.com>
2020-02-25 00:06:27 -0800

    Still rendering a cube, partially switched over to twgl

--- modules/scene5.js
+++ modules/scene5.js
@@ -9,41 +9,26 @@ const sceneId = '5';
 
 const vertShader = `#version 300 es
 in vec4 a_position;
-in vec3 a_normal;
 
 uniform mat4 u_modelMatrix;
 uniform mat4 u_viewMatrix;
 uniform mat4 u_projectionMatrix;
 
-out vec3 v_normal;
-
 void main() {
   mat4 mvp = u_projectionMatrix * u_viewMatrix * u_modelMatrix;
   gl_Position = mvp * a_position;
-  // Transpose of inverted model matrix, used for normals
-  mat4 modelInverseTranspose = transpose(inverse(u_modelMatrix));
-  v_normal = mat3(modelInverseTranspose) * a_normal;
 }
 `;
 
 const fragShader = `#version 300 es
 precision mediump float;
 
-uniform vec3 u_reverseLightDir;
-uniform vec4 u_lightColor;
 uniform vec4 u_matColor;
 
-in vec3 v_normal;
 out vec4 finalColor;
 
 void main() {
-  vec3 normal = normalize(v_normal);
-
-  // compute the light intensity - normal . reverse light
-  float intensity = dot(normal, u_reverseLightDir);
-  vec4 color = u_lightColor;
-  color.rgb *= intensity;
-  finalColor = color;
+  finalColor = vec4(1);
 }
 `;
 
@@ -52,9 +37,10 @@ const startTime = performance.now();
 export default function render () {
   const canvas = createCanvas(sceneId);
   const gl = initGL(canvas);
-  const shaderProgram = createShaders(gl, vertShader, fragShader);
+  const programInfo = createShaders(gl, vertShader, fragShader);
+  const shaderProgram = programInfo.program;
   const scene = createVertexData(gl, shaderProgram);
-  draw(gl, shaderProgram, scene, performance.now());
+  draw(gl, programInfo, scene, performance.now());
 }
 
 function initGL (canvas) {
@@ -66,74 +52,47 @@ function initGL (canvas) {
 }
 
 function createShaders (gl, vs, fs) {
-  const vertexShader = getShader(gl, gl.VERTEX_SHADER, vs);
-  const fragmentShader = getShader(gl, gl.FRAGMENT_SHADER, fs);
-  const shaderProgram = gl.createProgram();
+  const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
 
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-  gl.useProgram(shaderProgram);
-
-  return shaderProgram;
+  return programInfo;
 }
 
 function createVertexData (gl, program) {
-  const vertices = new Float32Array(cubeData.vertices);
-  const normals = new Float32Array(cubeData.normals);
-  const indices = new Uint16Array(cubeData.indices);
+  const positions = [1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1];
+  // const normals = [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1];
+  // const texcoords = [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1];
+  const indices = [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23];
 
+  // @TODO: why is this block crucial?
   const vertexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   const positionLoc = gl.getAttribLocation(program, 'a_position');
-  // Tell the attribute how to get data out of positionBuffer
+  // Tell the attribute how to get data out of vertexBuffer 
   gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(positionLoc);
 
-  // Create and populate normal buffer
-  var normalBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-
-  // Set attrib pointer for this buffer to the location of 'a_normal'
-  const normalLoc = gl.getAttribLocation(program, 'a_normal');
-  gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(normalLoc);
-
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  // Set up index buffer
-  const indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  var cubeBufferInfo = twgl.primitives.createCubeBufferInfo(gl, 1);
+  console.log(cubeBufferInfo);
 
   const scene = {
-    cube: indexBuffer,
+    cube: {
+      bufferInfo: cubeBufferInfo,
+    },
   };
   return scene;
 }
 
-function getShader (gl, type, shaderSource) {
-  var shader = gl.createShader(type);
-  gl.shaderSource(shader, shaderSource);
-  gl.compileShader(shader);
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw new Error(gl.getShaderInfoLog(shader));
-  }
-  return shader;
-}
-
-function drawCube (gl, program, scene, transform, viewMatrix, projMatrix) {
+function drawCube (gl, programInfo, scene, transform, viewMatrix, projMatrix) {
   const offset = 0;
   const count = 36;
   const color = new Vector4(0.2, 0.2, 0.2, 1.0);
   drawArbitraryTriangles(
     gl,
-    program,
+    programInfo,
     scene,
     transform,
     viewMatrix,
@@ -143,13 +102,13 @@ function drawCube (gl, program, scene, transform, viewMatrix, projMatrix) {
     color);
 }
 
-function drawPlane (gl, program, scene, transform, viewMatrix, projMatrix) {
+function drawPlane (gl, programInfo, scene, transform, viewMatrix, projMatrix) {
   const offset = 12;
   const count = 6;
   const color = new Vector4(0.2, 0.2, 0.2, 1.0);
   drawArbitraryTriangles(
     gl,
-    program,
+    programInfo,
     scene,
     transform,
     viewMatrix,
@@ -159,8 +118,8 @@ function drawPlane (gl, program, scene, transform, viewMatrix, projMatrix) {
     color);
 }
 
-function drawArbitraryTriangles (gl, program, scene, transform, viewMatrix, projMatrix, offset, count, color) {
-  var primitiveType = gl.TRIANGLES;
+function drawArbitraryTriangles (gl, programInfo, scene, transform, viewMatrix, projMatrix, offset, count, color) {
+  gl.bindVertexArray(scene.cube.VAO);
 
   const matrices = {
     u_modelMatrix: transform,
@@ -168,32 +127,17 @@ function drawArbitraryTriangles (gl, program, scene, transform, viewMatrix, proj
     u_projectionMatrix: projMatrix,
   };
   for (const name in matrices) {
-    const location = gl.getUniformLocation(program, name);
+    const location = gl.getUniformLocation(programInfo.program, name);
     gl.uniformMatrix4fv(location, false, matrices[name]);
   }
 
-  const vecs = {
-    u_lightColor: new Vector4([0.2, 1, 0.2, 1]),
-    u_reverseLightDir: new Vector3([0.1, 0.7, 1]),
-    u_matColor: new Vector4([0.1, 0.3, 1.0, 1]),
-  };
-  for (const name in vecs) {
-    const location = gl.getUniformLocation(program, name);
-    const vecLen = vecs[name].length;
-    if (vecLen === 3) {
-      gl.uniform3fv(location, vecs[name]);
-    } else if (vecLen === 4) {
-      gl.uniform4fv(location, vecs[name]);
-    }
-  }
+  twgl.setBuffersAndAttributes(gl, programInfo, scene.cube.bufferInfo);
 
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, scene.cube);
-  // offset needs to be multiplied by 2 since it's gl.UNSIGNED_SHORT
-  gl.drawElements(primitiveType, count, gl.UNSIGNED_SHORT, offset * 2);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  gl.useProgram(programInfo.program);
+  twgl.drawBufferInfo(gl, scene.cube.bufferInfo);
 }
 
-function drawScene (gl, projMatrix, viewMatrix, textureMatrix, program, scene, timestamp) {
+function drawScene (gl, projMatrix, viewMatrix, textureMatrix, programInfo, scene, timestamp) {
   const elapsedMs = timestamp - startTime;
   const msPerRotation = 8000;
   const rotationRadians = 2 * Math.PI * (elapsedMs / msPerRotation);
@@ -204,7 +148,7 @@ function drawScene (gl, projMatrix, viewMatrix, textureMatrix, program, scene, t
   cubeTransform.translate([0, 0, zCenter]);
   cubeTransform.rotateY(rotationRadians);
 
-  drawCube(gl, program, scene, cubeTransform, viewMatrix, projMatrix);
+  drawCube(gl, programInfo, scene, cubeTransform, viewMatrix, projMatrix);
 
   // Draw plane
   const planeTransform = new Matrix4().identity();
@@ -217,10 +161,10 @@ function drawScene (gl, projMatrix, viewMatrix, textureMatrix, program, scene, t
   planeTransform.translate([0, -1, 0]);
 
   // then translate downwards from the origin so we can actually see this
-  drawPlane(gl, program, scene, planeTransform, viewMatrix, projMatrix);
+  // drawPlane(gl, programInfo, scene, planeTransform, viewMatrix, projMatrix);
 }
 
-function draw (gl, program, scene, timestamp) {
+function draw (gl, programInfo, scene, timestamp) {
   const topDownCheckbox = document.getElementById(`topDownCheckbox${sceneId}`);
   const zoomSlider = document.getElementById(`zoomSlider${sceneId}`);
 
@@ -260,10 +204,10 @@ function draw (gl, program, scene, timestamp) {
   projMatrix.translate([0, -2, 0]); // @TODO: why does this need to be negative?
   projMatrix.rotateX(Math.PI / 20);
 
-  drawScene(gl, projMatrix, viewMatrix, null, program, scene, timestamp);
+  drawScene(gl, projMatrix, viewMatrix, null, programInfo, scene, timestamp);
 
   // gl.deleteProgram(program);
   requestAnimationFrame(function (timestamp) {
-    draw(gl, program, scene, timestamp);
+    draw(gl, programInfo, scene, timestamp);
   });
 }
