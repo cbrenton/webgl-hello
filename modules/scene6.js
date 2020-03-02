747cc5eb413338a61c4c2c1c97efc341a888684c
Chris Brenton <chrisbrenton@gmail.com>
2020-03-01 22:25:08 -0800

    Add texturedPhong shader

--- modules/scene6.js
+++ modules/scene6.js
@@ -21,6 +21,110 @@ shaders.phongShader = {
   vs: `#version 300 es
 in vec4 a_position;
 in vec3 a_normal;
+
+uniform mat4 u_modelMatrix;
+uniform mat4 u_viewMatrix;
+uniform mat4 u_projectionMatrix;
+uniform vec3 u_cameraPos;
+uniform vec3 u_lightPos;
+uniform mat4 u_depthVP;
+
+out vec3 v_normal;
+out vec3 v_viewVec;
+out vec4 v_shadowCoord;
+out vec3 v_lightPos;
+
+void main() {
+  mat4 mvp = u_projectionMatrix * u_viewMatrix * u_modelMatrix;
+  gl_Position = mvp * a_position;
+
+  mat4 modelInverseTranspose = transpose(inverse(u_modelMatrix));
+  v_normal = mat3(modelInverseTranspose) * a_normal;
+
+  vec3 surfaceWorldPos = vec3(u_modelMatrix * a_position);
+  v_lightPos = u_lightPos;
+  v_viewVec= u_cameraPos - surfaceWorldPos;
+  v_shadowCoord = u_depthVP * u_modelMatrix * a_position;
+}`,
+  fs: `#version 300 es
+precision mediump float;
+
+in vec3 v_normal;
+in vec3 v_viewVec;
+in vec4 v_shadowCoord;
+in vec3 v_lightPos;
+
+uniform float u_shadowMapSize;
+uniform float u_shininess;
+uniform vec3 u_diffuseColor;
+uniform vec3 u_specularColor;
+uniform vec3 u_ambientColor;
+uniform vec3 u_lightColor;
+uniform sampler2D u_shadowMap;
+uniform bool u_useSoftShadows;
+
+out vec4 finalColor;
+
+void main() {
+  vec3 normal = normalize(v_normal);
+
+  // Convert 3D world position to clip space (0 to 1)
+  vec3 shadowCoord = v_shadowCoord.xyz / v_shadowCoord.w;
+
+  float cameraDepth = shadowCoord.z;
+  float shadowMapDepth = texture(u_shadowMap, shadowCoord.xy).r;
+
+  float visibility = 0.0;
+  float bias = 0.00002;
+
+  if (cameraDepth - shadowMapDepth < bias) {
+    visibility += 1.0;
+  }
+
+  if (u_useSoftShadows) {
+    float count = 1.0;
+    for (float y = -1.5; y <= 1.5; y += 1.0) {
+      for (float x = -1.5; x <= 1.5; x += 1.0) {
+        vec2 shadowMapSize = 1.0f / vec2(u_shadowMapSize);
+        vec2 texOffset = vec2(x * shadowMapSize.x, y * shadowMapSize.y);
+        float tmpShadowDepth = texture(u_shadowMap, shadowCoord.xy + texOffset).r;
+        if (cameraDepth - tmpShadowDepth < bias) {
+          visibility += 1.0;
+        }
+        count += 1.0;
+      }
+    }
+    visibility /= count;
+  }
+
+  vec3 L = normalize(v_lightPos);
+  vec3 N = normal;
+  vec3 V = normalize(v_viewVec);
+  vec3 R = -reflect(L, N);
+
+  // Diffuse
+  vec3 diffuse = u_lightColor * dot(L, N) * u_diffuseColor;
+
+  // Specular
+  float specularStrength = 1.0;
+  vec3 specular = vec3(0);
+  if (dot(R, V) > 0.0) {
+    specular = specularStrength * pow(dot(R, V), u_shininess) * u_lightColor * u_specularColor;
+  }
+
+  // Ambient
+  float ambientStrength = 0.1;
+  vec3 ambient = ambientStrength * u_lightColor * u_ambientColor;
+
+  vec3 result = (ambient + (specular + diffuse) * visibility);;
+  finalColor = vec4(result, 1.0);
+}`,
+};
+
+shaders.texturedPhongShader = {
+  vs: `#version 300 es
+in vec4 a_position;
+in vec3 a_normal;
 in vec2 a_texcoord;
 
 uniform mat4 u_modelMatrix;
@@ -230,11 +334,13 @@ export default function render() {
   const gl = util.createGLCanvas(sceneId, description);
   const programInfos = {
     phong: util.createShaders(gl, shaders.phongShader),
+    texturedPhong: util.createShaders(gl, shaders.texturedPhongShader),
     checkerboard: util.createShaders(gl, shaders.planeShader),
     hud: util.createShaders(gl, shaders.hudShader),
   };
   const depthProgramInfos = {
     phong: util.createShaders(gl, shaders.simpleColorShader),
+    texturedPhong: util.createShaders(gl, shaders.simpleColorShader),
     checkerboard: util.createShaders(gl, shaders.simpleColorShader),
     hud: util.createShaders(gl, shaders.simpleColorShader),
   }
@@ -294,7 +400,6 @@ function createScene(gl) {
         specular: new Vector3([0.2, 0.8, 0.2]),
         ambient: new Vector3([0.6, 0.6, 0.6]),
       },
-
       shininess: 16.0,
       texture: scene.textures.blankTexture,
     }
@@ -460,7 +565,6 @@ function drawScene(gl, programInfos, scene, renderCamera, lightVP, timestamp) {
     u_diffuseColor: scene.sphere.material.color.diffuse,
     u_specularColor: scene.sphere.material.color.specular,
     u_ambientColor: scene.sphere.material.color.ambient,
-    u_texture: scene.sphere.material.texture,
     u_shininess: scene.sphere.material.shininess,
   };
   util.drawBuffer(
@@ -474,7 +578,6 @@ function drawScene(gl, programInfos, scene, renderCamera, lightVP, timestamp) {
     u_diffuseColor: scene.cube.material.color.diffuse,
     u_specularColor: scene.cube.material.color.specular,
     u_ambientColor: scene.cube.material.color.ambient,
-    u_texture: scene.cube.material.texture,
     u_shininess: scene.cube.material.shininess,
   };
   util.drawBuffer(
@@ -491,7 +594,7 @@ function drawScene(gl, programInfos, scene, renderCamera, lightVP, timestamp) {
     u_shininess: scene.plane.material.shininess,
   };
   util.drawBuffer(
-      gl, programInfos.phong, scene.plane.bufferInfo, planeUniforms,
+      gl, programInfos.texturedPhong, scene.plane.bufferInfo, planeUniforms,
       globalUniforms);
 }
 
